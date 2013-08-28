08a1974b250d6622e25f0e64591180d6f279c6eb
xiaodingbian <xiaodingbian@vmware.com>
2013-08-28 20:03:15 +0800

    generate data disks, swap disk and hadoop log dir info based on disk UUID fetched from WS to solve disk dis-order issue

--- lib/ironfan/common/server.rb
+++ lib/ironfan/common/server.rb
@@ -39,19 +39,38 @@ module Ironfan
 
       def sync_volume_attributes
         super
-
         return if fog_server.nil? or fog_server.volumes.nil? or fog_server.volumes.empty?
         mount_point_to_device = {}
         device_to_disk = {}
+        swap_disk = nil
         fog_server.volumes.each do |disk|
-          # disk should equal to '/dev/sdb' or 'dev/sdc', etc.
-          device = disk + '1'
-          mount_point = '/mnt/' + device.split('/').last
-          mount_point_to_device[mount_point] = device
-          device_to_disk[device] = disk
+          if disk.start_with?("DATA")
+            # disk uuid fetch from WS is: DATA:6000C298-2b5d-f41a-2581-1b07e74971e8
+            # disk uuid shown in OS is: scsi-36000c2982b5df41a25811b07e74971e8
+            # the logic partion is: scsi-36000c2982b5df41a25811b07e74971e8-part1
+            uuid_in_os = "scsi-3" + disk.split(":")[1].gsub("-", "").downcase
+            raw_disk = "/dev/disk/by-id/" + uuid_in_os
+            device = "/dev/disk/by-id/" + uuid_in_os + "-part1"
+            mount_point = '/mnt/' + uuid_in_os + "-part1"
+            mount_point_to_device[mount_point] = device
+            device_to_disk[device] = raw_disk
+          else
+            swap_disk = "/dev/disk/by-id/scsi-3" + disk.split(":")[1].gsub("-", "").downcase
+          end
         end
         @chef_node.normal[:disk][:data_disks]  = mount_point_to_device
         @chef_node.normal[:disk][:disk_devices] = device_to_disk
+        @chef_node.normal[:disk][:swap_disk] = swap_disk
+
+        # cannot gurantee node[:disk][:data_disks].keys.last can always get the same 
+        # disk considering cluster fix feature, so we need to save root log dir in Chef to make 
+        # sure hadoop log is written to the same place after reboot.
+        # Another issue is "cluster fix" may delete the disk where root log dir 
+        # locates on.
+        pre_log_root_dir = @chef_node.normal[:disk][:hadoop_log_root_dir]
+        if pre_log_root_dir.nil? or !mount_point_to_device.has_key?(pre_log_root_dir)
+          @chef_node.normal[:disk][:hadoop_log_root_dir] = mount_point_to_device.keys.last
+        end
       end
     end
   end
